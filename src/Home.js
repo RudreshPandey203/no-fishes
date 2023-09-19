1f2504b46f57513a4826ff9648babda24fd5ff55
Rudresh <rudreshpandey@Rudreshs-MacBook-Air.local>
2023-09-19 07:26:11 +0530

    first commit

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button, Nav } from 'react-bootstrap'
+
+import './Home.css'
+
+function Home() {
+  return (
+    <div className='home'>
+        <Nav></Nav>
+    </div>
+  )
+}
+
+export default Home
